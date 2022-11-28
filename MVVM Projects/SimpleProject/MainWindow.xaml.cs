192b835ec687a640a69e7f84f881cde293c48773
Bechir Bejaoui <me780411@gmail.com>
2022-11-28 10:55:44 +0100

    Ajout du premier projet dans le module MVVM

---
+++ MVVM Projects/SimpleProject/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IModel _context;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string connectionMode = ConfigurationManager.AppSettings["choix"].ToString();
+            Choix choix;
+            if (connectionMode == "EF")
+            { 
+                choix = Choix.EF; 
+            }
+            else
+            {
+                choix = Choix.Liste;
+            }
+
+            switch (choix)
+            {
+                case Choix.EF:
+                    _context = new AdventureWorksLT2014Entities();
+                    dgdata.ItemsSource = _context.Products.ToList();
+                    break;
+                case Choix.Liste:
+                    _context = new ProductList();
+                    dgdata.ItemsSource = (List<Product>)_context;
+                    break;
+                default:
+                    _context = new ProductList();
+                    dgdata.ItemsSource = (List<Product>)_context;
+                    break;
+            }
+        } 
+    }
+
+    public enum Choix
+    {
+        EF, Liste
+    }
+}
---
+++ MVVM Projects/SimpleProject/ProductList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleProject
+{
+    public class ProductList :List<Product>,IModel
+    {
+        public ProductList()
+        {
+            Add(new Product { ProductID = 1, Name = "ProduitA", Color = "Red" });
+            Add(new Product { ProductID = 2, Name = "ProduitB", Color = "Green" });
+            Add(new Product { ProductID = 3, Name = "ProduitC", Color = "Blue" });
+            Add(new Product { ProductID = 4, Name = "ProduitD", Color = "Yellow" });
+
+            //Ajoute du code pour alimenter Products
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
