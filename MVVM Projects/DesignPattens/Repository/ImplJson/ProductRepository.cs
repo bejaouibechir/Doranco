996d47c6d2824573be6e9edd86f62ce7717d1df5
Bechir Bejaoui <me780411@gmail.com>
2022-11-29 11:48:22 +0100

    Ajout du mécanisme d'accès aux données JSON

--- MVVM Projects/DesignPattens/Repository/ImplJson/ProductRepository.cs
+++ MVVM Projects/DesignPattens/Repository/ImplJson/ProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DesignPattens.Repository.Abstraction;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,37 @@ using System.Threading.Tasks;
 
 namespace DesignPattens.Repository.ImplJson
 {
-    public class ProductRepository
+    public class ProductRepository : IRepository<Product>
     {
+        JsonModel<Product> _context;
 
+        public ProductRepository()
+        {
+            _context = new JsonModel<Product>();
+        }
+        public void Add(Product entity)
+        {
+            _context.Add(entity);
+        }
+
+        public void Delete(Product entity)
+        {
+            _context.Delete(entity.ProductID);
+        }
+
+        public Product Get(int id)
+        {
+            return _context.Get(id);
+        }
+
+        public List<Product> List()
+        {
+            return _context.GetAll();
+        }
+
+        public void Update(Product entity)
+        {
+            _context.Update(entity.ProductID, entity);
+        }
     }
 }
