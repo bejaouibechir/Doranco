828f07022b6751900e439c24a3129bc7bc390875
Bechir Bejaoui <me780411@gmail.com>
2022-12-12 10:04:52 +0100

    Ajout de solution pour DEV 132

---
+++ Dev132/Dev132WPFSolution/ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+
+        static string chaine = string.Empty; // => "  " 
+        static Stopwatch chrono = new Stopwatch();
+        static StringBuilder stringBuilder = new StringBuilder(chaine);
+        
+        static void Main(string[] args)
+        {
+             
+            chrono.Start();
+            for (int i = 0; i < 20000; i++)
+            {
+                chaine = chaine + (i*10).ToString();
+            }
+            chrono.Stop();
+            string durée_execution1 = chrono.Elapsed.TotalMilliseconds.ToString();
+            
+            chrono.Reset();
+
+            chrono.Start();
+            for (int i = 0; i < 20000; i++)
+            {
+                stringBuilder.Append((i * 10).ToString());
+            }
+            chrono.Stop();
+            string durée_execution2 = chrono.Elapsed.TotalMilliseconds.ToString();
+
+        }
+    }
+}
